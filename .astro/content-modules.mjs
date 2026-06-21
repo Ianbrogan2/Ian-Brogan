c38330a38c76a8645719ac4cd25f802c153ff6ab
Ianbrogan2 <ian.alexander.brogan@icloud.com>
2026-06-21 00:31:50 -0700

    n

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,5 @@
 
 export default new Map([
-["content/journal/the-year-i-built-things.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fjournal%2Fthe-year-i-built-things.mdx&astroContentModuleFlag=true")],
-["content/journal/learning-the-market-at-ten.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fjournal%2Flearning-the-market-at-ten.mdx&astroContentModuleFlag=true")]]);
+["content/journal/learning-the-market-at-ten.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fjournal%2Flearning-the-market-at-ten.mdx&astroContentModuleFlag=true")],
+["content/journal/the-year-i-built-things.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fjournal%2Fthe-year-i-built-things.mdx&astroContentModuleFlag=true")]]);
 		
